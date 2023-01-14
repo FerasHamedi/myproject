04e987a810a637cb0a5e6bf9476dbf1c35d0b9c0
feras <feras.5f5.ff@gmail.com>
2023-01-13 22:54:59 -0800

    mymaster

---
+++ lib/first_page.dart
@@ -0,0 +1,16 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class firstPage extends StatelessWidget {
+  const firstPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(),
+      body: (Text("data")),
+    );
+  }
+}
